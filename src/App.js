57420a83609fca20cd35109681d07c19baf48cf0
Manish Poduval <manishpoduval@hotmail.com>
2020-11-06 19:40:23 +0530

    socket-client

--- src/App.js
+++ src/App.js
@@ -1,25 +1,175 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+import "./App.css";
+
+let socket;
+const CONNECTION_PORT = "localhost:3002/";
 
 function App() {
+  // Before Login
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [room, setRoom] = useState("");
+  const [userName, setUserName] = useState("");
+
+  // After Login
+  const [message, setMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  useEffect(() => {
+    socket = io(CONNECTION_PORT);
+  }, [CONNECTION_PORT]);
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageList([...messageList, data]);
+    });
+  });
+  const connectToRoom = () => {
+    setLoggedIn(true);
+    socket.emit("join_room", room);
+  };
+
+  const sendMessage = async () => {
+    let messageContent = {
+      room: room,
+      content: {
+        author: userName,
+        message: message,
+      },
+    };
+
+    await socket.emit("send_message", messageContent);
+    setMessageList([...messageList, messageContent.content]);
+    setMessage("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!loggedIn ? (
+        <div className="logIn">
+          <div className="inputs">
+            <input
+              type="text"
+              placeholder="Name..."
+              onChange={(e) => {
+                setUserName(e.target.value);
+              }}
+            />
+            <input
+              type="text"
+              placeholder="Room..."
+              onChange={(e) => {
+                setRoom(e.target.value);
+              }}
+            />
+          </div>
+          <button onClick={connectToRoom}>Enter Chat</button>
+        </div>
+      ) : (
+        <div className="chatContainer">
+          <div className="messages">
+            {messageList.map((val, key) => {
+              return (
+                <div
+                  className="messageContainer"
+                  id={val.author == userName ? "You" : "Other"}
+                >
+                  <div className="messageIndividual">
+                    {val.author}: {val.message}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+
+          <div className="messageInputs">
+            <input
+              type="text"
+              placeholder="Message..."
+              onChange={(e) => {
+                setMessage(e.target.value);
+              }}
+            />
+            <button onClick={sendMessage}>Send</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
+
+// import React, { useState, useEffect } from 'react'
+// import io from 'socket.io-client'
+// import TextField from '@material-ui/core/TextField'
+// import './App.css'
+
+// const socket = io.connect('http://localhost:5000')
+
+// function App() {
+//   const [state, setStaet] = useState({ message: '', name: '' })
+//   const [chat, setChat] = useState([])
+
+//   useEffect(() => {
+//     socket.on('message', ({ name, message }) => {
+//       setChat([...chat, { name, message }])
+//     })
+//   })
+
+//   const onTextChange = e => {
+//     setStaet({ ...state, [e.target.name]: e.target.value })
+//   }
+
+//   const onMessageSubmit = e => {
+//     e.preventDefault()
+//     const { name, message } = state
+//     socket.emit('message', { name, message })
+//     setStaet({ message: '', name })
+//   }
+
+//   const renderChat = () => {
+//     return chat.map(({ name, message }, index) => (
+//       <div key={index}>
+//         <h3>
+//           {name}: <span>{message}</span>
+//         </h3>
+//       </div>
+//     ))
+//   }
+
+//   return (
+//     <div className="card">
+//       <form onSubmit={onMessageSubmit}>
+//         <h1>Messanger</h1>
+//         <div className="name-field">
+//           <TextField
+//             name="name"
+//             onChange={e => onTextChange(e)}
+//             value={state.name}
+//             label="Name"
+//           />
+//         </div>
+//         <div>
+//           <TextField
+//             name="message"
+//             onChange={e => onTextChange(e)}
+//             value={state.message}
+//             id="outlined-multiline-static"
+//             variant="outlined"
+//             label="Message"
+//           />
+//         </div>
+//         <button>Send Message</button>
+//       </form>
+//       <div className="render-chat">
+//         <h1>Chat Log</h1>
+//         {renderChat()}
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default App
